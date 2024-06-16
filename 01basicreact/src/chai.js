5b8d998c38da29cc55b767c0141937f47705e777
Prashant Mishra <140933262+PrashantMishra10232@users.noreply.github.com>
2024-06-16 04:37:36 +0000

    nbx

---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <p>My name is Dolly chai wala</p>
+    )
+}
+
+export default Chai;
--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import Chai from './chai';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    <Chai/>
+  </ React.StrictMode>
 );
 
 
