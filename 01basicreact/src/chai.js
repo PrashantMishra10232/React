554dc8c14dda220a8d5338330d5d7f6511e32759
Prashant Mishra <140933262+PrashantMishra10232@users.noreply.github.com>
2024-06-16 04:47:44 +0000

    do

--- 01basicreact/src/chai.js
+++ 01basicreact/src/chai.js
@@ -1,7 +1,20 @@
+// react hume js mein direct html likhne deti hai
+
+//yaha kuch rules lgte hein
+//1: file k naam capital rakhna jruri nhi h pr ek achi practice hai 
+//2: function ka naam capital letter se hi start krne compulsory hai chahe vite library use kro  ya basic react ki
+//3: function bnane ke baad usse same js ya jsx file m export krna compulsory hai
+//4: phir is function file ko main.js ya index.js m import krna bhi compulsory hai
+//5: return m ek time pr bs ek hi element daal skte h 
+//6: agr ek se jyada elements dalne h toh unhe ek <div> mein store kro ya ek empty element m <></>
+
+
 function Chai(){
     return(
         <p>My name is Dolly chai wala</p>
     )
 }
+
+
 
 export default Chai;
--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -8,7 +8,7 @@ import Chai from './chai';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> 
     <Chai/>
   </ React.StrictMode>
 );
